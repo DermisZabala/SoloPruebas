968d2bd75aef1ccaf2b1924d9470e6776b4b4cde
DermisZabala <dermiszabala@gmail.com>
2025-07-05 22:07:05 -0400

    Mejorando archivos de vercel

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
+# build_files.sh
+
+# Salir inmediatamente si un comando falla
 set -e
-echo "Compilando SASS..."
-sass static/scss:static/css
-echo "Minificando JS..."
-uglifyjs static/js/main.js -o static/js/main.min.js
-echo "Recolectando estáticos..."
-python manage.py collectstatic --noinput
+
+echo "BUILD START"
+
+# 1. Instalar dependencias de Python
+echo "Instalando dependencias de Python..."
+pip install -r requirements.txt
+
+# 2. Recolectar archivos estáticos de Django
+echo "Ejecutando collectstatic..."
+# Usamos python3.12 para ser explícitos, ya que es el runtime de Vercel
+python3.12 manage.py collectstatic --no-input --clear
+
+echo "BUILD END"
