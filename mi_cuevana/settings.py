811eafbb976dfd99567a6901f558e42ea967a485
DermisZabala <dermiszabala@gmail.com>
2025-07-05 21:35:17 -0400

    Mejorando archivos de vercel

--- mi_cuevana/settings.py
+++ mi_cuevana/settings.py
@@ -141,7 +141,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Configuración para que WhiteNoise encuentre los archivos estáticos en producción
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
